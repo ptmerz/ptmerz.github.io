ef494c45b8e3719369c53f4b0ca437127b572d99
ptmerz <ptmerz@users.noreply.github.com>
2021-12-09 19:11:12 +0000

    Deploy commit 08aeb9852e7374a02f199f09e93884203e6e94e7
    Autodeployed using __EdricChan03_action-build-deploy-ghpages in Deployment

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Minimal Mistakes and MathJax",
-        "excerpt":"After setting up this website with the beautiful Minimal Mistakes theme, I noticed that it wasn’t supporting typesetting math equations. Well, it can’t be too hard to get this to work, right? Right… Note that I am using Minimal Mistakes as a remote theme, and I would really like to...","categories": [],
-        "tags": [],
-        "url": "/mmistakes-mathjax/",
+        "excerpt":"After setting up this website with the beautiful Minimal Mistakes theme, I noticed that it wasn’t supporting typesetting math equations. Well, it can’t be too hard to get this to work, right? Right… Note that I am using Minimal Mistakes as a remote theme, and I would really like to...","categories": ["Blogging platform"],
+        "tags": ["Minimal Mistakes","Jekyll","MathJax"],
+        "url": "/blog/mmistakes-mathjax/",
         "teaser": null
       }]
